e0c836a6c5c36fc00d0511259f65e10d0c7ce6d9
shuden1 <102833873+shuden1@users.noreply.github.com>
2024-08-07 10:14:16 +0400

    cleanup

--- ParkerScripts/ClutchCollector/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('start').addEventListener('click', function() {
-        chrome.runtime.sendMessage({action: "start"});
-    });
-    document.getElementById('downloadButton').addEventListener('click', function() {
-        chrome.runtime.sendMessage({action: "download"});
-    });
-});
-
